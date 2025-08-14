d6a5205cff478379e062991e2c544364e1a5e052
mattmchugh4 <114327452+mattmchugh4@users.noreply.github.com>
2025-08-14 00:02:57 -0400

    Add Node.js to Homebrew package installation in install.sh

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ COMMON_BREW_PACKAGES=(
   stow       # Symlink farm manager
   thefuck    # Corrects your previous console command
   tree       # Display directories as trees
+  node       # Node.js
   pnpm       # Node package manager
 )
 
